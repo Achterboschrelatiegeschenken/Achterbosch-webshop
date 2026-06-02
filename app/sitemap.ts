fd1ef53a57a36e17b2691edc85dfc6cac5495d37
Patrick Achterbosch <p.achterbosch@gmail.com>
2026-06-02 15:01:50 +0200

    Sitemap toegevoegd

---
+++ app/sitemap.ts
@@ -0,0 +1,34 @@
+import { MetadataRoute } from "next"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.achterboschrelatiegeschenken.nl",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.achterboschrelatiegeschenken.nl/assortiment",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.achterboschrelatiegeschenken.nl/winkelwagen",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.achterboschrelatiegeschenken.nl/checkout",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.achterboschrelatiegeschenken.nl/producten/borrelplank",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.achterboschrelatiegeschenken.nl/producten/pennen",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.achterboschrelatiegeschenken.nl/producten/houten-flesopener",
+      lastModified: new Date(),
+    },
+  ]
+}
